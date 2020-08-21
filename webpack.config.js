f91928eecb1338951dbaa765787a79d317592be5
lysak.ilya <ilsy@brighta.biz>
2020-08-21 21:25:07 +0300

    header

--- webpack.config.js
+++ webpack.config.js
@@ -75,15 +75,15 @@ const plugins = () => {
             }
         }),
         new CleanWebpackPlugin(),
-        // new CopyPlugin({
-        //     patterns: [
-        //         {
-        //             from: path.resolve(__dirname, './src/img'),
-        //             to: path.resolve(__dirname, './dist/img')
-        //         },
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './src/img'),
+                    to: path.resolve(__dirname, './docs/img')
+                },
 
-        //     ]
-        // }),
+            ]
+        }),
         new MiniCssExtractPlugin({
             filename: filename('css')
         })
